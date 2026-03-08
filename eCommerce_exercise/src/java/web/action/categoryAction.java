6393f3c9896296319c077e6a2a99fb7ed62878d1
Alicia Lázaro Huerta <81562520+alissssia@users.noreply.github.com>
2026-03-08 16:42:13 +0100

    Add files via upload

---
+++ eCommerce_exercise/src/java/web/action/categoryAction.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.action;
+
+import javax.servlet.http.*;
+import model.CategoryModel;
+import model.ProductModel;
+import web.ViewManager;
+
+/**
+ *
+ * @author entel
+ */
+public class categoryAction implements Action {
+    private CategoryModel categoryModel;
+    private ProductModel productModel;
+    
+    public categoryAction(CategoryModel categoryModel, ProductModel productModel) {
+        this.categoryModel = categoryModel;
+        this.productModel = productModel;
+    }
+    
+    @Override
+    public void perform(HttpServletRequest req, HttpServletResponse resp) {
+        int categoryId = Integer.parseInt(req.getParameter("categoryid"));
+        
+        req.setAttribute("categories", categoryModel.retrieveAll());
+        req.setAttribute("products", productModel.retrieveByCategory(categoryId));
+        ViewManager.nextView(req, resp, "/view/category.jsp");
+    }
+}
